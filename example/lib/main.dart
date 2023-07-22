07fe9c4621bef4349583e3660c24bea574084b15
Nialixus <nialixus@gmail.com>
2023-07-22 12:31:27 +0700

    Initial Commit

---
+++ example/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(home: ExamplePage()));
+}
+
+class ExamplePage extends StatelessWidget {
+  const ExamplePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
